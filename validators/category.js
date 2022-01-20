ed976443cd11136deffb93f6b384338f064edb9f
Facundo Juarez <facu.j.94@gmail.com>
2022-01-20 14:01:36 -0300

    Add createCategory validator

---
+++ validators/category.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const {check, validationResult} = require('express-validator');
+
+exports.createCategoryValidator = [
+    check('name')
+        .trim()
+        .escape()
+        .isLength({min: 2, max: 30})
+        .withMessage('Name length should be between 2 and 30 characters'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(422).json({errors: errors.array()});
+        next();
+    },
+];
